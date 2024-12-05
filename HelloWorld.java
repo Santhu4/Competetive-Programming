02ffca7d94ccae09c0f8375936c42b0c0a036fc2
santhosh rao <santhoshraotk14@gmail.com>
2024-12-05 11:19:22 +0530

    comments added

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld{
         System.out.println("Hello Java World");
         System.out.println("Hellow branch switching");
         System.out.println("Starting the merging process");
+        // comment added
     }
 }
